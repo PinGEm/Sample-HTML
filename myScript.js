e52221f6c2a39fe14b71787d4455cff56e08c35f
Marcus <silvergreat0@gmail.com>
2025-01-21 13:47:08 +0800

    website update
    website update

---
+++ myScript.js
@@ -0,0 +1,7 @@
+function buttonPressed(){
+    const button_Header = document.getElementById("button_header");
+    const button = document.getElementById("button");
+
+    button_Header.innerText = "Are you sure you're not lying?";
+    button.innerText = "Just trust me bro";
+}
